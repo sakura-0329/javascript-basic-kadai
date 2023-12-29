ea1618903480a9ab9dddf2ff0b94670e628d40b1
sakura-0329 <147309375+sakura-0329@users.noreply.github.com>
2023-12-29 22:11:57 +0900

    Add files via upload

--- kadai_014/kadai_014/square.js
+++ kadai_014/kadai_014/square.js
@@ -1,6 +1,8 @@
 const argument = (square) => {
-  //与えられた引数squareに2乗し、その値を出力する
-  console.log(square*2);
+  //与えられた引数squareを2乗にし、その値を戻り値として返す関数を定義する
+  return square*2;
+ 
   }
 
-  argument(50);
+  // 関数の戻り値を出力する
+  console.log(argument(50));
