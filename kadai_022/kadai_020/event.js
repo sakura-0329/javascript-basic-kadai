4efdf56e85f1cb7bc98509b404ad1de1e7b82885
sakura-0329 <147309375+sakura-0329@users.noreply.github.com>
2024-01-01 10:57:05 +0900

    Add files via upload

---
+++ kadai_022/kadai_020/event.js
@@ -0,0 +1,16 @@
++// head要素を取得し、中身を出力する
++console.log(document.head);
++// body要素を取得し、中身を出力する
++console.log(document.body);
+// windowオブジェクトの中身を出力する
+console.log(window);
+
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+text.textContent = 'ボタンをクリックしました';
+  });
